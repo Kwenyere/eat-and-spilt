f8f977bda6b924e727086b05b822c1fa873273a8
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-10-27 11:29:59 +0300

    implement controlled element on form

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -4,6 +4,8 @@ import Button from "./Button";
 export default function FormSplitBill({ selectedFriend }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("");
   return (
     <form className="form-split-bill">
       <h2>Split Bill With {selectedFriend.name}</h2>
@@ -18,14 +20,19 @@ export default function FormSplitBill({ selectedFriend }) {
       <input
         type="text"
         value={paidByUser}
-        onChange={(e) => setPaidByUser(+e.target.value)}
+        onChange={(e) =>
+          setPaidByUser(+e.target.value > bill ? paidByUser : +e.target.value)
+        }
       ></input>
 
       <label>👬 {selectedFriend.name} Expenses:</label>
-      <input type="text" disabled></input>
+      <input type="text" disabled value={paidByFriend}></input>
 
       <label>😉 Who is paying?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
